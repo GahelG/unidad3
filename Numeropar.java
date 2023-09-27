dae047074d828fe97d29cc884252f6f0131aaba6
GahelG <143622147+GahelG@users.noreply.github.com>
2023-09-26 19:40:25 -0600

    Add files via upload

---
+++ Numeropar.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numeropar;
+import java.util.Scanner;
+
+/**
+ *
+ * @author holak
+ */
+public class Numeropar {
+
+    public static void main(String[] args) {
+       
+        Scanner ent=new Scanner(System.in);
+                
+                int N,O,R;
+                System.out.println("ingresa un número ");
+                N=ent.nextInt();
+                
+                if (N % 2 ==0){
+                    System.out.println("El número es par ");
+                    
+                     }
+                    else {
+                    System.out.println("El número es impar");
+                }
+                ent.close();
+    }
+}
